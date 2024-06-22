4f1a3d54428922f4bed150c81139dd34971880b9
Mohamed Mohamed Ebrahim <70168921+mohamedmohamed201295@users.noreply.github.com>
2024-06-22 10:13:07 +0300

    Update rule.js

--- routes/validation-layer/rule.js
+++ routes/validation-layer/rule.js
@@ -1,30 +1,30 @@
-import pkg from "express-validator";
-import validator from "./validator.js";
+import pkg from 'express-validator'
+import validator from './validator.js'
 
 const createTaskValidation = [
-  pkg.body("name", "Task name is required").trim().notEmpty(),
-  validator,
-];
+  pkg.body('name', 'Task name is required').trim().notEmpty(),
+  validator
+]
 
 const getTaskValidation = [
-  pkg.param("id", `No item found with this id`).isMongoId(),
-  validator,
-];
+  pkg.param('id', `No item found with this id`).isMongoId(),
+  validator
+]
 
 const editTaskValidation = [
-  pkg.param("id", `No item found with this id`).isMongoId(),
-  pkg.body("name", "Task name is required").trim().notEmpty(),
-  validator,
-];
+  pkg.param('id', `No item found with this id`).isMongoId(),
+  pkg.body('name', 'Task name is required').trim().notEmpty(),
+  validator
+]
 
 const deleteTaskValidation = [
-  pkg.param("id", `No item found with this id`).isMongoId(),
-  validator,
-];
+  pkg.param('id', `No item found with this id`).isMongoId(),
+  validator
+]
 
-export default {
+export {
   createTaskValidation,
   getTaskValidation,
   editTaskValidation,
-  deleteTaskValidation,
-};
+  deleteTaskValidation
+}
