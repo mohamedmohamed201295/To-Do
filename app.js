f56573ad64439e6fdf1ea507b4991a9ab9cc5cf2
Mohamed Mohamed Ebrahim <70168921+mohamedmohamed201295@users.noreply.github.com>
2024-06-22 10:14:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-import dotenv from "dotenv";
-dotenv.config();
-import "express-async-errors";
+import { fileURLToPath } from 'url'
 
-import express from "express";
-const app = express();
+import 'dotenv/config'
+import 'express-async-errors'
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
 
-import tasksRouter from "./routes/tasks.js";
+import tasksRouter from './routes/tasks.js'
+import connectDB from './db/connect.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
-import connectDB from "./db/connect.js";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
 
-import errorHandlerMiddleware from "./middleware/error-handler.js";
-
-import morgan from "morgan";
-
-app.use(morgan("combined"));
-console.log(`mode: ${process.env.NODE_ENV}`);
+app.use(morgan('combined'))
+console.log(`mode: ${process.env.NODE_ENV}`)
 
 // middleware
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 
 // routes
 
-app.use("/api/v1/tasks", tasksRouter);
+app.use('/api/v1/tasks', tasksRouter)
 
 // error handler
-app.use(errorHandlerMiddleware);
-app.use((req, res) => res.status(404).send("Route does not exist"));
+app.use(errorHandlerMiddleware)
+app.use((req, res) => res.status(404).send('Route does not exist'))
 
 // connectDB
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-connectDB(process.env.MONGO_URI);
+connectDB(process.env.MONGO_URI)
 const server = app.listen(
   port,
   console.log(`Server is listening on port ${port}...`)
-);
+)
 
-process.on("unhandledRejection", (err) => {
-  console.log(`Unhandled Rejection Error: ${err.name} | ${err.message}`);
+process.on('unhandledRejection', err => {
+  console.log(`Unhandled Rejection Error: ${err.name} | ${err.message}`)
   server.close(() => {
-    console.log("shutting down....");
-    process.exit(1);
-  });
-});
+    console.log('shutting down....')
+    process.exit(1)
+  })
+})
