87de55fe012793d98693154606ee249e3c4f1ca4
Mohamed Mohamed Ebrahim <70168921+mohamedmohamed201295@users.noreply.github.com>
2024-06-22 10:10:32 +0300

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,54 +1,48 @@
-import Task from "../models/Task.js";
-import CustomAPIError from "../errors/custom-api.js";
+import Task from '../models/Task.js'
+import CustomAPIError from '../errors/custom-api.js'
 
 const getAllTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.status(200).json({ tasks, noHits: tasks.length });
-};
+  const tasks = await Task.find()
+  res.status(200).json({ tasks, noHits: tasks.length })
+}
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
-};
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+}
 
 const getTask = async (req, res, next) => {
-  const { id: taskID } = req.params;
-  const task = await Task.findOne({ _id: taskID });
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
   if (!task) {
-    throw new CustomAPIError(`No task with id : ${taskID}`, 404);
+    throw new CustomAPIError(`No task with id : ${taskID}`, 404)
   }
 
-  res.status(200).json({ task });
-};
+  res.status(200).json({ task })
+}
 
 const deleteTask = async (req, res, next) => {
-  const { id: taskID } = req.params;
-  const task = await Task.findOneAndDelete({ _id: taskID });
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
   if (!task) {
-    throw new CustomAPIError(`No task with id : ${taskID}`, 404);
+    throw new CustomAPIError(`No task with id : ${taskID}`, 404)
   }
-  res.status(200).json({ task });
-};
+  res.status(200).json({ task })
+}
 
 const editTask = async (req, res, next) => {
-  const { id: taskID } = req.params;
+  const { id: taskID } = req.params
 
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
-    runValidators: true,
-  });
+    runValidators: true
+  })
 
   if (!task) {
-    throw new CustomAPIError(`No task with id : ${taskID}`, 404);
+    throw new CustomAPIError(`No task with id : ${taskID}`, 404)
   }
 
-  res.status(200).json({ task });
-};
+  res.status(200).json({ task })
+}
 
-export default {
-  getAllTasks,
-  createTask,
-  getTask,
-  editTask,
-  deleteTask,
-};
+export { getAllTasks, createTask, getTask, editTask, deleteTask }
